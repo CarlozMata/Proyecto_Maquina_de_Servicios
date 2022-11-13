d2a10c764b29496a8d110783e9c18ebd23fd1a26
Carlos Alberto López Mata <mata66666666666@gmail.com>
2022-11-13 12:40:47 -0600

    update

--- src/JS/index.js
+++ src/JS/index.js
@@ -1,2 +1,20 @@
-window.alert("Hello world!");
-alert("Hello world!..");
+function guardarDatos() {
+  const user = {
+  const name = document.getElementById('name').value;
+  const firstName = document.getElementById('firstName').value;
+  }
+  let text = name;
+
+  document.getElementById('textForm').innerHTML = text;
+/*
+  if (name=="Carlos" &&& "Alberto") {
+    document.getElementById('textForm').innerHTML = "Tu eres Carlos Mata";
+  } else {
+    document.getElementById('textForm').innerHTML = "Tu no eres Carlos..";
+  }*/
+  const userVerify = (name) => {
+    if (!name.enabled) return "El usuario no esta habilitado"; 
+
+    return name
+  }
+}
