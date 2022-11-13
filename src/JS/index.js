2f4944ffee0fccb5f26f55ca4e3173aecda201eb
Carlos Alberto López Mata <mata66666666666@gmail.com>
2022-11-13 13:58:26 -0600

    update

--- src/JS/index.js
+++ src/JS/index.js
@@ -1,16 +1,37 @@
 function guardarDatos() {
 
-  let name = document.getElementById('name').value;
-  let firstName = document.getElementById('firstName').value;
-  let email = document.getElementById('email').value;
+  const usuario = {
+    nombre: "Carlos",
+    apellido: "Lopez",
+    correo : "carlos_1992_@hotmail.com"
+  };
 
-  let text = "Tú nombre es: " + name + " " + firstName + " y el correo es " + email;
+  const userRegistrado = {
+   name : document.getElementById('name').value,
+   firstName : document.getElementById('firstName').value,
+   email : document.getElementById('email').value
+  }
 
-  document.getElementById('textForm').innerHTML = text;
+//  let text = "Tú nombre es: " + name + " " + firstName + " y el correo es " + email;
+//  document.getElementById('textForm').innerHTML = text;
 /*
-  if (name=="Carlos" &&& "Alberto") {
+  if (userRegistrado.name === usuario.nombre) {
     document.getElementById('textForm').innerHTML = "Tu eres Carlos Mata";
   } else {
     document.getElementById('textForm').innerHTML = "Tu no eres Carlos..";
-  }*/
-}
+  }
+*/
+  if (userRegistrado.name === usuario.nombre)
+  return document.getElementById('textForm').innerHTML = `Tu nombre es ${userRegistrado.name} ${userRegistrado.firstName} con correo electronico: ${userRegistrado.email} y si te encuentras registrado :D`;
+  return userRegistrado.name === "" ? document.getElementById('textForm').innerHTML = "No estas registrado, prueba poniendo nombre: Carlos, Apellido: Lopez y Correo: carlos_1992_@hotmail.com y funcionara :)" : "Hola mundo";
+
+
+
+/*
+  const userVerify = (userRegistrado) => {
+    if (!userRegistrado.nombre === "") return  document.getElementById('textForm').innerHTML = "No ingreso un nombre";
+    return userRegistrado.name === usuario.nombre ? `Hola mi nombre es ${usuario.nombre}` : "No existe usuario";
+  };
+  userVerify(userRegistrado);
+
+*/}
