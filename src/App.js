09614cee59efec9697dc09e144ec7afc8c91f138
Carlos Alberto López Mata <mata66666666666@gmail.com>
2022-11-13 11:25:04 -0600

    update

--- src/App.js
+++ src/App.js
@@ -4,21 +4,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <nav>
-    <h1 class="title-nav">PAGO DE SERVICIOS CON REACT</h1>
-  </nav>
-  <section class="section-dp">
-    <div class="section-div-dp">
-      <h2>Ingresa tus datos:</h2>
-      <label>Nombre(s): <input type="text" id="Name" name="Name" placeholder="Ingresa tú nombre" /></label>
-      <label>Apellidos: <input type="text" id="firstname" name="firstname" placeholder="Ingresa tú apellido"/></label>
-      <label>Correo Electronico: <input type="text" id="email" name="email" placeholder="Ingresa tu Correo"/></label>
-      <div class="section-div-dp-btn">
-        <button>Btn</button>
-      </div>
-  </div>
-  </section>
-  <footer>Footer</footer>
 </header>
     </div>
   );
