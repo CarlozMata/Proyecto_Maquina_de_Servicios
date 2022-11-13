d2276723959cb2618b4a5a66ad2c5164e9ccfc1c
Carlos Alberto López Mata <mata66666666666@gmail.com>
2022-11-13 12:50:47 -0600

    update

--- src/JS/index.js
+++ src/JS/index.js
@@ -1,8 +1,6 @@
 function guardarDatos() {
-  const user = {
-  const name = document.getElementById('name').value;
-  const firstName = document.getElementById('firstName').value;
-  }
+  let name = document.getElementById('name').value;
+
   let text = name;
 
   document.getElementById('textForm').innerHTML = text;
@@ -14,7 +12,6 @@ function guardarDatos() {
   }*/
   const userVerify = (name) => {
     if (!name.enabled) return "El usuario no esta habilitado"; 
-
-    return name
+    
   }
 }
