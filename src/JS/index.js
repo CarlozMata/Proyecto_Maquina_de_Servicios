cc17eb3ba05f629f807f741516c5285e0ecda4ce
Carlos Alberto López Mata <mata66666666666@gmail.com>
2022-11-15 13:37:07 -0600

    update

--- src/JS/index.js
+++ src/JS/index.js
@@ -14,27 +14,10 @@ function guardarDatos() {
   email : document.getElementById('email').value
   }
 
-//  let text = "Tú nombre es: " + name + " " + firstName + " y el correo es " + email;
-//  document.getElementById('textForm').innerHTML = text;
-/*
-  if (userRegistrado.name === usuario.nombre) {
-    document.getElementById('textForm').innerHTML = "Tu eres Carlos Mata";
-  } else {
-    document.getElementById('textForm').innerHTML = "Tu no eres Carlos..";
-  }
-*/
-  if (userRegistrado.name === usuario.nombre)
+  if (userRegistrado.name === usuario.nombre && userRegistrado.firstName === usuario.apellido && userRegistrado.email === usuario.correo)
   return document.getElementById('textForm').innerHTML = `Tu nombre es ${userRegistrado.name} ${userRegistrado.firstName} con correo electronico: ${userRegistrado.email} y si te encuentras registrado :D`;
-  return userRegistrado.name === "" ? document.getElementById('textForm').innerHTML = "No estas registrado, prueba poniendo nombre: Carlos, Apellido: Lopez y Correo: carlos_1992_@hotmail.com y funcionara :)" : "Hola mundo";
-
-/*
-  const userVerify = (userRegistrado) => {
-    if (!userRegistrado.nombre === "") return  document.getElementById('textForm').innerHTML = "No ingreso un nombre";
-    return userRegistrado.name === usuario.nombre ? `Hola mi nombre es ${usuario.nombre}` : "No existe usuario";
-  };
-  userVerify(userRegistrado);
-
-*/}
+  return userRegistrado.name === "" ? document.getElementById('textForm').innerHTML = "No estas registrado, prueba poniendo nombre: Carlos, Apellido: Lopez y Correo: carlos_1992_@hotmail.com y funcionara :)" : document.getElementById('textForm').innerHTML = "Usuario Erroneo";
+}
 
 //FORMULARIO 2
 
@@ -45,9 +28,38 @@ function metodosString () {
 
   const texto = document.getElementById('cadenaTexto').value
 
-
   const metodoLength = texto.length
-  window.alert(metodoLength)
-  metodoLength();
-  window.alert(metodoLength)
+//  const resultadoLength = metodoLength
+//  window.alert(resultadoLength)
+//  document.getElementById("metodoLength").appendChild(metodoLength);
+
+
+  const node = document.createElement("span");
+  const textnode = document.createTextNode(metodoLength);
+  node.appendChild(textnode);
+  document.getElementById("metodoLength").appendChild(node);
+
+  document.getElementById("metodoLength").appendChild(node);
+
 }
+
+/* CHEMA!!!
+const addResult = (element, result) => {
+  const node = document.createElement("span");
+  const textnode = document.createTextNode(result);
+  node.appendChild(textnode);
+  document.getElementById(element).appendChild(node);
+}
+
+
+
+
+//
+
+contar elemtnso de un array:
+
+const size = [].lenght
+
+addResult("arraySize", size)
+addResult("arraySize", size)
+*/
